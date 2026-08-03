76bcd7de0c5123ca12b072d28f4177e701fa52ef
uploadersoasis <72340531+uploadersoasis@users.noreply.github.com>
2026-08-03 13:52:34 -0500

    Delete script7.js
    no longer relevant

--- script7.js
+++
@@ -1,8 +0,0 @@
-/// override-let-func.js
-/// alias override-let-func
-/// world MAIN
-function overrideletfunc(selector) {
-    if (arguments) {
-        console.info(`Argument 1 is ${arguments[0]}.`);
-    }
-}
