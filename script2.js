bd52e39f49d673be7a0355a29902c77a05db83e0
uploadersoasis <72340531+uploadersoasis@users.noreply.github.com>
2026-08-03 03:08:36 -0500

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,23 +1,7 @@
 /// override-let-func.js
 /// alias override-let-func
-/// world MAIN
-
+/// world ISOLATED
 function overrideletfunc(selector = '') {
-    if (!selector) { return; }
-
-    const hook = () => {
-        if (typeof window[selector] === 'function') {
-            const original = window[selector];
-            window[selector] = function(...args) {
-                console.log('[uBO Intercept]', selector, args);
-                return original.apply(this, args);
-            };
-        }
-    };
-
-    if (document.readyState === 'loading') {
-        document.addEventListener('DOMContentLoaded', hook, { once: true });
-    } else {
-        hook();
-    }
+    calculateArea2 = console.log(`Hey bitch, it's ${calculateArea2.name}!`);
+    calculateArea2();
 }
