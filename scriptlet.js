594af070620b451dee29cd1a5688ae743dda5f1c
uploadersoasis <72340531+uploadersoasis@users.noreply.github.com>
2026-08-02 23:54:42 -0500

    Add files via upload

--- scriptlet.js
+++ scriptlet.js
@@ -1,23 +1,22 @@
-'use strict';
-
 /// override-let-func.js
 /// alias override-let-func
 /// dependency run-at.fn
 /// world ISOLATED
 //  example.com##+js(override-let-func, window.alert)
+//'use strict';
 function overrideletfunc(selector = '') {
-    selector = '{{1}}'; // e.g., "calculateArea"
-// Checks if the argument was left empty or wasn't provided at all
+    selector = '{{1}}'; // e.g., "calculateArea2"
+    // Checks if the argument was left empty or wasn't provided at all
     if (!selector || selector === '{{1}}') { return; }
     runAt(( ) => {
         try {
             const scriptElem = document.createElement('script');
-            // Executes in the root page scope (outside our IIFE)
+            // Constructs and injects raw script into the page root scope
             scriptElem.textContent = `
-                if (typeof ${selector} === 'function') {
+                if (typeof ${selector} !== 'undefined') {
                     const original_${selector} = ${selector};
                     ${selector} = function(...args) {
-                        console.log('Intercepted call to ${selector}:', args);
+                        console.log('[uBO] Intercepted call to ${selector}:', args);
                         return original_${selector}.apply(this, args);
                     };
                 }
@@ -27,5 +26,5 @@ function overrideletfunc(selector = '') {
         } catch (err) {
             console.error(err?.message);
         }
-    }, 'complete');
+    }, 'idle');  // start, interactive, idle are the three choices
 }
